1f18b4b3fac63b85f2f1c5a6b4144ff01841fe9c
tripathisana04 <147637295+tripathisana04@users.noreply.github.com>
2023-10-11 20:57:48 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World!..Azure cloud computing is very cool")
